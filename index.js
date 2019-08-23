7967a9a2a60a8bb2a0047e99cfe27a72c3217329
Marce Graffigna <marcesmartphone@gmail.com>
2019-08-23 17:27:08 +0100

    index.js updated mongodb url

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv')
+dotenv.config()
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const routerv1 = require('./src/v1/routes')
 
-const mongoUrl = 'mongodb://localhost:27017'
 const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(express.urlencoded())
 app.use('/api/v1', routerv1)
 
-mongoose.connect(mongoUrl + "/dbej2").then(res => {
+mongoose.connect(process.env.MONGODB_URI + "/dbej2").then(res => {
     app.listen(PORT, HOST);
     console.log(`Running on http://${HOST}:${PORT}`);
 }).catch(err => {
